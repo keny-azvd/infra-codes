1cdeda09fa8b759a8ef39e0314c5bdf7142307ab
Kenner Azevedo <53868486+keny-azvd@users.noreply.github.com>
2024-11-22 01:46:59 -0300

    Add files via upload

---
+++ reboot_machines.ps1
@@ -0,0 +1,27 @@
+﻿# Caminho para o arquivo com as máquinas
+$filePath = "path"
+
+# Verifica se o arquivo existe
+if (-Not (Test-Path $filePath)) {
+    Write-Host "Arquivo $filePath não encontrado." -ForegroundColor Red
+    exit
+}
+
+# Lê cada linha do arquivo
+$machines = Get-Content $filePath
+
+foreach ($hhost in $machines) {
+    Write-Host "Conectando-se a $hhost..." -ForegroundColor Cyan
+
+    try {
+         #Reinicia a máquina remotamente usando a credencial atual
+        Invoke-Command -ComputerName $hhost -ScriptBlock {
+            Restart-Computer -Force
+        }
+
+        Write-Host "Máquina $hhost reiniciada com sucesso." -ForegroundColor Green
+    } catch {
+        Write-Host "Erro ao conectar/reiniciar $hhost  $_" -ForegroundColor Red
+    }
+}
+
